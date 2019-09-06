4eb05119525c2be1ebdf015faac53e6b9076a49d
Fabricio <fabriciojacob124@gmail.com>
2019-09-06 15:11:20 -0300

    Implementação dos exemplos no curso.

--- cadastro-clientes/src/main/kotlin/br/com/curso/cadastroclientes/CadastroClientesApplication.kt
+++ cadastro-clientes/src/main/kotlin/br/com/curso/cadastroclientes/CadastroClientesApplication.kt
@@ -2,8 +2,10 @@ package br.com.curso.cadastroclientes
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient
 
 @SpringBootApplication
+@EnableDiscoveryClient
 class CadastroClientesApplication
 
 fun main(args: Array<String>) {
