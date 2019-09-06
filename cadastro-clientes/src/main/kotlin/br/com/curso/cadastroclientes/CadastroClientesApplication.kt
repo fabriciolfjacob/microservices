a3443b11305259ad88d40c5ca2bef2238ae260ac
Fabricio <fabriciojacob124@gmail.com>
2019-09-06 14:21:09 -0300

    init project.

---
+++ cadastro-clientes/src/main/kotlin/br/com/curso/cadastroclientes/CadastroClientesApplication.kt
@@ -0,0 +1,11 @@
+package br.com.curso.cadastroclientes
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class CadastroClientesApplication
+
+fun main(args: Array<String>) {
+	runApplication<CadastroClientesApplication>(*args)
+}
---
+++ cadastro-clientes/src/test/kotlin/br/com/curso/cadastroclientes/CadastroClientesApplicationTests.kt
@@ -0,0 +1,16 @@
+package br.com.curso.cadastroclientes
+
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.test.context.junit4.SpringRunner
+
+@RunWith(SpringRunner::class)
+@SpringBootTest
+class CadastroClientesApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
